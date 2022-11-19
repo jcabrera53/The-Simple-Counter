9c8835b86d5b76e31d4805267f0f73e917469c77
jcabrera53 <juancabrera53@gmail.com>
2022-11-19 16:34:00 +0000

    primeros pasos del simple counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,24 +3,26 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
+
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">This is my Simple Counter</h1><br/>
+			<div className="d-flex justify-content-around" id="BigCounter">
+			<div className="Clock">
+				<i className="fa-regular fa-clock"></i>
+			</div>
+			<div className="Four">{props.digitFour}</div>
+			<div className="Three">{props.digitThree}</div>
+			<div className="Two">{props.digitTwo}</div>
+			<div className="One">{props.digitOne}</div>
+			</div>
 		</div>
 	);
 };
+
+setInterval(function(){},1000);
 
 export default Home;
